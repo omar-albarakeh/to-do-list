60c18fa70058ff74fe451fdb5f419b55e325a41f
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-09 23:18:37 +0200

    different linear gradient methods used

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -19,25 +19,59 @@ class _HomepageState extends State<Homepage> {
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
             colors: [Colors.orange, Colors.red],
-          )
-          ),
+          )),
           child: AppBar(
             backgroundColor: Colors.transparent,
-            title: Text("To-Do List" , style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold)),
+            title: Text("To-Do List",
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
             centerTitle: true,
           ),
         ),
       ),
-      body:Column(
+      body: Column(
         children: [
-
+          ShaderMask(
+            shaderCallback: (Rect bounds) {
+              return LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.orange, Colors.red],
+              ).createShader(bounds);
+            },
+            child: Text(
+              "Gradient Text",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){},
-        child: Icon(Icons.add),
-        backgroundColor: Colors.orangeAccent,
-        
+      floatingActionButton: Material(
+        shape: CircleBorder(),
+        elevation: 4,
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            width: 56,
+            height: 56,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.orange, Colors.red],
+              ),
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
